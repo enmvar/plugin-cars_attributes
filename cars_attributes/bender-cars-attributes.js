25a08bb10ee287ce623e34c26f9bb30586074e0a
mmcsus <mmcsus@gmail.com>
2013-08-27 16:08:50 -0500

    Create bender-cars-attributes.js

---
+++ cars_attributes/bender-cars-attributes.js
@@ -0,0 +1,241 @@
+$('#make').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs2').text(''); 
+     var selectVal = $('#make :selected').text();
+     $("#mmcs2").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#make').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs2').text(''); 
+     var selectVal = $('#make :selected').text();
+     $("#mmcs2").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#model').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs3').text(''); 
+     var selectVal = $('#model :selected').text();
+     $("#mmcs3").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#model').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs3').text(''); 
+     var selectVal = $('#model :selected').text();
+     $("#mmcs3").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#car_type').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs4').text(''); 
+     var selectVal = $('#car_type :selected').text();
+     $("#mmcs4").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#car_type').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs4').text(''); 
+     var selectVal = $('#car_type :selected').text();
+     $("#mmcs4").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#doors').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs5').text(''); 
+     var selectVal = $('#doors :selected').text();
+     $("#mmcs5").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#doors').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs5').text(''); 
+     var selectVal = $('#doors :selected').text();
+     $("#mmcs5").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#seats').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs6').text(''); 
+     var selectVal = $('#seats :selected').text();
+     $("#mmcs6").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#seats').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs6').text(''); 
+     var selectVal = $('#seats :selected').text();
+     $("#mmcs6").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#num_airbags').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs7').text(''); 
+     var selectVal = $('#num_airbags :selected').text();
+     $("#mmcs7").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#num_airbags').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs7').text(''); 
+     var selectVal = $('#num_airbags :selected').text();
+     $("#mmcs7").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#transmission').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs8').text(''); 
+     var selectVal = $('#transmission :selected').text();
+     $("#mmcs8").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#transmission').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs8').text(''); 
+     var selectVal = $('#transmission :selected').text();
+     $("#mmcs8").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#fuel').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs9').text(''); 
+     var selectVal = $('#fuel :selected').text();
+     $("#mmcs9").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#fuel').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs9').text(''); 
+     var selectVal = $('#fuel :selected').text();
+     $("#mmcs9").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#seller').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs10').text(''); 
+     var selectVal = $('#seller :selected').text();
+     $("#mmcs10").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#seller').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs10').text(''); 
+     var selectVal = $('#seller :selected').text();
+     $("#mmcs10").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#power_unit').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs11').text(''); 
+     var selectVal = $('#power_unit :selected').text();
+     $("#mmcs11").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#power_unit').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs11').text(''); 
+     var selectVal = $('#power_unit :selected').text();
+     $("#mmcs11").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+
+$('#gears').ready(function() {
+ 
+   // assign the value to a variable 
+     $('#mmcs12').text(''); 
+     var selectVal = $('#gears :selected').text();
+     $("#mmcs12").append(selectVal);
+  
+    //alert(selectVal);
+
+});
+ 
+$('#gears').change(function() {
+
+   // assign the value to a variable 
+     $('#mmcs12').text(''); 
+     var selectVal = $('#gears :selected').text();
+     $("#mmcs12").append(selectVal);
+  
+    //alert(selectVal);
+
+});
