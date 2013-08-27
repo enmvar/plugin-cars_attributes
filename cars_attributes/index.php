9466bdbda75569f454d83878ab1a2cc4940b28d5
mmcsus <mmcsus@gmail.com>
2013-08-27 15:42:46 -0500

    Update index.php

--- cars_attributes/index.php
+++ cars_attributes/index.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name:  Cars Attributes
+Plugin Name: Cars attributes
 Plugin URI: http://www.osclass.org/
 Description: This plugin extends a category of items to store cars attributes such as model, year, brand, color, accessories, and so on.
 Version: 3.0.4
 Author: OSClass
 Author URI: http://www.osclass.org/
 Short Name: cars_plugin
-Plugin update URI:  
+Plugin update URI: cars-attributes
 */
     require_once 'ModelCars.php' ;
 
